0c9ca8f9bbdcd409b31b9a4f3ecd69165dd5cd6c
voomdoon <nilreb24-github20141105@yahoo.de>
2023-04-30 17:59:10 +0200

    test: test logger

--- src/test/java/de/voomdoon/util/cli/ProgramTest.java
+++ src/test/java/de/voomdoon/util/cli/ProgramTest.java
@@ -8,6 +8,9 @@ import java.util.concurrent.atomic.AtomicInteger;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import de.voomdoon.logging.LogEvent;
+import de.voomdoon.logging.LogLevel;
+import de.voomdoon.testing.logging.tests.LoggingCheckingTestBase;
 import de.voomdoon.testing.tests.TestBase;
 
 /**
@@ -17,7 +20,35 @@ import de.voomdoon.testing.tests.TestBase;
  *
  * @since 0.1.0
  */
-class ProgramTest {
+class ProgramTest extends LoggingCheckingTestBase {
+
+	/**
+	 * DOCME add JavaDoc for ProgramTest.RunTest
+	 *
+	 * @author André Schulz
+	 *
+	 * @since 0.1.0
+	 */
+	public static class TestProgram extends Program {
+
+		/**
+		 * DOCME add JavaDoc for constructor TestProgram
+		 * 
+		 * @param args
+		 * @since 0.1.0
+		 */
+		protected TestProgram(String[] args) {
+			super(args);
+		}
+
+		/**
+		 * @since 0.1.0
+		 */
+		@Override
+		protected void runProgram() throws Exception {
+			// nothing to do
+		}
+	}
 
 	/**
 	 * DOCME add JavaDoc for ProgramTest
@@ -107,5 +138,63 @@ class ProgramTest {
 			assertThat(TestProgram.INSTANCE).isPresent();
 			assertThat(TestProgram.INSTANCE.get().runProgramCallCount).hasValue(1);
 		}
+	}
+
+	/**
+	 * DOCME add JavaDoc for method test_logger
+	 * 
+	 * @since 0.1.0
+	 */
+	@Test
+	void testName() throws Exception {
+		logTestStart();
+
+		TestProgram program = new TestProgram(new String[0]);
+		program.logger.debug("test");
+
+		assertThat(getLogCache().getLogEvents(LogLevel.DEBUG)).hasSize(1);
+		assertThat(getLogCache().getLogEvents(LogLevel.DEBUG).get(0)).extracting(LogEvent::getMessage)
+				.isEqualTo("test");
+	}
+
+	/**
+	 * DOCME add JavaDoc for method getLogEvent
+	 * 
+	 * @param timestamp
+	 * @param sourceClass
+	 * @param level
+	 * @param message
+	 * @return
+	 * @since 0.1.0
+	 */
+	private LogEvent getLogEvent(long timestamp, Class<?> sourceClass, LogLevel level, Object message) {
+		return new LogEvent() {
+
+			@Override
+			public Throwable getError() {
+				// TODO implement getError
+				throw new UnsupportedOperationException("'getError' not implemented at 'Type1682868671772'!");
+			}
+
+			@Override
+			public LogLevel getLevel() {
+				return level;
+			}
+
+			@Override
+			public Object getMessage() {
+				return message;
+			}
+
+			@Override
+			public Class<?> getSourceClass() {
+				return sourceClass;
+			}
+
+			@Override
+			public long getTimestamp() {
+				return timestamp;
+			}
+		};
 	}
 }
