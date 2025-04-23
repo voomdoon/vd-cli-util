1f445c912d1e1201130515715d4f2b8524c31884
voomdoon <nilreb24-github20141105@yahoo.de>
2025-04-23 18:25:07 +0200

    test: refactor: use TempInputDirectory

--- src/test/java/de/voomdoon/util/cli/FilesProcessorTest.java
+++ src/test/java/de/voomdoon/util/cli/FilesProcessorTest.java
@@ -11,9 +11,13 @@ import java.util.List;
 import java.util.function.Consumer;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
+import de.voomdoon.testing.file.TempFileExtension;
+import de.voomdoon.testing.file.TempInputDirectory;
+import de.voomdoon.testing.file.WithTempInputDirectories;
 import de.voomdoon.testing.logging.tests.LoggingCheckingTestBase;
 
 /**
@@ -25,6 +29,8 @@ import de.voomdoon.testing.logging.tests.LoggingCheckingTestBase;
  * @deprecated
  */
 @Deprecated
+@ExtendWith(TempFileExtension.class)
+@WithTempInputDirectories(create = true)
 class FilesProcessorTest extends LoggingCheckingTestBase {
 
 	/**
@@ -33,11 +39,8 @@ class FilesProcessorTest extends LoggingCheckingTestBase {
 	 * @since 0.1.0
 	 */
 	@Test
-	void testProcessFiles_directory() throws Exception {
+	void testProcessFiles_directory(@TempInputDirectory File directory) throws Exception {
 		logTestStart();
-
-		File directory = new File(getTempDirectory() + "/directory");
-		directory.mkdir();
 
 		Path file1 = Path.of(directory.toString(), "test1.file");
 		Files.writeString(file1, "test");
@@ -67,8 +70,8 @@ class FilesProcessorTest extends LoggingCheckingTestBase {
 	 * @since 0.1.0
 	 */
 	@Test
-	void testProcessFiles_file() throws IOException {
-		Path file = Path.of(getTempDirectory().toString(), "test.file");
+	void testProcessFiles_file(@TempInputDirectory File directory) throws IOException {
+		Path file = Path.of(directory.toString(), "test.file");
 		Files.writeString(file, "test");
 
 		@SuppressWarnings("unchecked")
